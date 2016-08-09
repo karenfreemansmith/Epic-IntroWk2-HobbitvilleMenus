d946c076040a0d2bb46188db8de0a8b25e7eac3b
Karen Freeman-Smith and Zachary Matthewstearn <zachary@>
2016-08-09 11:02:49 -0700

    add styles to change backgrounds

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+$("#sb").click(function() {
+  $("body").removeClass();
+  $("body").addClass("sb");
+  $("li").removeClass();
+  $("#sb").addClass("active");
+});
+
+$("#csb").click(function() {
+  $("body").removeClass();
+  $("body").addClass("csb");
+  $("li").removeClass();
+  $("#csb").addClass("active");
+});
+
+$("#tc").click(function() {
+  $("body").removeClass();
+  $("body").addClass("tc");
+  $("li").removeClass();
+  $("#tc").addClass("active");
+});
+
 $(".breakfast .clickable").click(function(event) {
   event.preventDefault();
   $(".breakfast .initially-hidden").animate({
@@ -29,6 +50,54 @@ $(".elevenses .clickable").click(function(event) {
     height: "toggle"
   });
   $(".elevenses .initially-hidden").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+});
+
+$(".luncheon .clickable").click(function(event) {
+  event.preventDefault();
+  $(".luncheon .initially-showing").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+  $(".luncheon .initially-hidden").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+});
+
+$(".afternoon-tea .clickable").click(function(event) {
+  event.preventDefault();
+  $(".afternoon-tea .initially-showing").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+  $(".afternoon-tea .initially-hidden").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+});
+
+$(".dinner .clickable").click(function(event) {
+  event.preventDefault();
+  $(".dinner .initially-showing").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+  $(".dinner .initially-hidden").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+});
+
+$(".supper .clickable").click(function(event) {
+  event.preventDefault();
+  $(".supper .initially-showing").animate({
+    opacity: "toggle",
+    height: "toggle"
+  });
+  $(".supper .initially-hidden").animate({
     opacity: "toggle",
     height: "toggle"
   });
